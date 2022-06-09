ac83565ccab565d17944c3c4ea3f11111ef3d3c8
Ken <fieldken@hotmail.com>
2022-06-09 10:51:41 +0200

    Add project files.

---
+++ MijnEersteWebApp/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MijnEersteWebApp.Models;
+
+namespace MijnEersteWebApp.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult About()
+        {
+            //return View(new InfoViewModel { Age = 26, FirstName = "Ken", LastName = "Field" });
+            var vm = new InfoViewModel { Age = 16, FirstName = "Ken", LastName = "Field" };
+            return View(vm);
+        }
+    }
+}
---
+++ MijnEersteWebApp/Models/InfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MijnEersteWebApp.Models
+{
+    public class InfoViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }   
+        public int Age { get; set; }
+
+        //public bool Adult { get { return Age >= 18; } }
+        public bool Adult => Age >= 18;
+    }
+}
